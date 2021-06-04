bef1e9701af2f35fa141440f77e9ccae7b4cb4a7
GeorgeGevorkyan <ggevorkyan@intermedia.net>
2021-06-04 15:04:55 +0300

    fixing

--- oidc/code-identityserver-sample.js
+++ oidc/code-identityserver-sample.js
@@ -1,9 +1,6 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-
-document.getElementById('getVoiceMails').addEventListener("click", GetVoiceMails, false);
-document.getElementById('getVoiceMailsToken').addEventListener("click", getAccessToken("api.user.voice.voicemails"), false);
 ///////////////////////////////
 // config
 ///////////////////////////////
@@ -11,9 +8,9 @@ Oidc.Log.logger = console;
 Oidc.Log.level = Oidc.Log.DEBUG;
 console.log("Using oidc-client version: ", Oidc.Version);
 access_token = null;
-var url = location.href.substring(0, location.href.lastIndexOf('/'));
+let url = location.href.substring(0, location.href.lastIndexOf('/'));
 
-var settings = {
+let settings = {
     authority: localStorage.getItem('cfg-authority'),
     client_id: localStorage.getItem('cfg-clientId'),
     redirect_uri: location.href.split('?')[0],
@@ -24,6 +21,7 @@ var settings = {
     login_hint: localStorage.getItem('cfg-login'),
     extraTokenParams: { acr_values: localStorage.getItem('cfg-acr') }
 };
+
 access_token = null;
 
 function getAccessToken(scope){
@@ -41,10 +39,10 @@ function getAccessToken(scope){
 // functions for VoiceMails
 ///////////////////////////////
 
-function GetVoiceMails()
+function getVoiceMails()
 {
     let theUrl = 'https://api.intermedia.net/voice/v2/voicemails?offset=0&count=100';
-    var xmlHttp = new XMLHttpRequest();
+    let xmlHttp = new XMLHttpRequest();
 
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
@@ -77,3 +75,7 @@ if (location.search.includes("code=", 1)) {
         log(err);
 });
 }
+
+
+document.getElementById('getVoiceMails').addEventListener("click", getVoiceMails, false);
+document.getElementById('getVoiceMailsToken').addEventListener("click", getAccessToken("api.user.voice.voicemails"), false);
