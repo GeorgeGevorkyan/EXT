127dac45c40cfb7057fc73a23d7b9031177fa2f9
GeorgeGevorkyan <ggevorkyan@intermedia.net>
2021-06-04 15:28:26 +0300

    fixing

--- oidc/code-identityserver-sample.js
+++ oidc/code-identityserver-sample.js
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 document.getElementById('getVoiceMails').addEventListener("click", getVoiceMails, false);
-document.getElementById('getVoiceMailsToken').addEventListener("click", getAccessToken("api.user.voice.voicemails"), false);
+document.getElementById('getVoiceMailsToken').addEventListener("click",() => { getAccessToken("api.user.voice.voicemails"); }, false);
 
 ///////////////////////////////
 // config
