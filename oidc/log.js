30769a3fdce9001deff7ebff4db5b7979ca77533
GeorgeGevorkyan <ggevorkyan@intermedia.net>
2021-07-05 11:52:25 +0300

    fixing

--- oidc/log.js
+++ oidc/log.js
@@ -1,11 +1,11 @@
 function log() {
-    Array.prototype.forEach.call(arguments, function(msg) {
+    Array.prototype.forEach.call(outName, function(msg) {
         if (msg instanceof Error){
             msg = "Error: " + msg.message;
         }
         else if (typeof msg !== 'string') {
             msg = JSON.stringify(msg, null, 2);
         }
-        document.getElementById('out').innerHTML += msg + '\r\n';
+        document.getElementById(outName).innerHTML += msg + '\r\n';
     });
 }
