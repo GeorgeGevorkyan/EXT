e20b6dedaffa41b2661cbd062995977672bb591f
GeorgeGevorkyan <ggevorkyan@intermedia.net>
2021-07-07 12:15:56 +0300

    fixing

--- oidc/code-identityserver-sample.js
+++ oidc/code-identityserver-sample.js
@@ -23,7 +23,13 @@ let settings = {
 ///////////////////////////////
 
 function getAccessToken(scope){
+    settings.authority = localStorage.getItem('cfg-authority');
+    settings.client_id = localStorage.getItem('cfg-clientId');
+    settings.acr_values = localStorage.getItem('cfg-acr');
+    settings.login_hint = localStorage.getItem('cfg-login');
+    settings.extraTokenParams = { acr_values: localStorage.getItem('cfg-acr') }
     settings.scope = scope;
+    
     let mgr = new Oidc.UserManager(settings);
     console.log("Going to sign in using following configuration", settings);
     mgr.signinRedirect({useReplaceToNavigate:true}).then(function() {
