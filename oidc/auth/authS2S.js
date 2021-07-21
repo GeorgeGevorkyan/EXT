b1add637da66a01c3d5a1b9a39d01989f839b6dd
GeorgeGevorkyan <ggevorkyan@intermedia.net>
2021-07-21 17:22:34 +0300

    fixing

--- oidc/auth/authS2S.js
+++ oidc/auth/authS2S.js
@@ -11,15 +11,17 @@ async function getAnalyticToken(client_id, client_secret){
             '&client_secret=' + client_secret +
             '&scope=api.service.analytics.main'
     };
-
+    log("1");
     fetch(url, options)
     .then((response) => response.json())
     .then((response) => {
         //localStorage.setItem('analytics_token', response['access_token']);
+        log("2");
         log("Token " + response['access_token']);
         return response['access_token'];
     })
     .catch((error) => {
         console.log(error);
     });;
+    log("3");
 }
