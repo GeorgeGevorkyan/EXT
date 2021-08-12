0003a77bb175d5b9734f0c87b0a03002d20aa5f8
GeorgeGevorkyan <84922844+GeorgeGevorkyan@users.noreply.github.com>
2021-08-12 05:21:04 -0700

    Add files via upload

--- auth/pkce/assets/redirect-page.js
+++ auth/pkce/assets/redirect-page.js
@@ -1,32 +1,32 @@
 ///////////////////////////////
 // IIFE on load (get access token)
 ///////////////////////////////
-(()=>{
-    if(isAuthorized()){
+(() => {
+    if (isAuthorized()) {
         window.location.href = "api-menu.html";
     }
 
     Oidc.Log.logger = console;
     Oidc.Log.level = Oidc.Log.DEBUG;
     console.log("Using oidc-client version: ", Oidc.Version);
-    
+
     let settings = {
         authority: localStorage.getItem('cfg-authority'),
         client_id: localStorage.getItem('cfg-clientId'),
         redirect_uri: location.href.split('?')[0],
         response_type: 'code',
         scope: localStorage.getItem('cfg-scopes'),
-    
-        automaticSilentRenew:false,
+
+        automaticSilentRenew: false,
         validateSubOnSilentRenew: false,
-    
-        monitorAnonymousSession : false,
+
+        monitorAnonymousSession: false,
         filterProtocolClaims: false,
         monitorSession: false,
         loadUserInfo: false,
-        revokeAccessTokenOnSignout : true,
-    
-        acr_values : localStorage.getItem('cfg-acr'),
+        revokeAccessTokenOnSignout: true,
+
+        acr_values: localStorage.getItem('cfg-acr'),
         login_hint: localStorage.getItem('cfg-login'),
         extraTokenParams: { acr_values: localStorage.getItem('cfg-acr') }
     };
@@ -35,7 +35,7 @@
         setSessionToken(response);
         window.location.href = "api-menu.html";
     }).catch((error) => {
-        console.log("Error!!! " + error);
         window.location.href = "auth.html";
+        log(error);
     });
 })();
